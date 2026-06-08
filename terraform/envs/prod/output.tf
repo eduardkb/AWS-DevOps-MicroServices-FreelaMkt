4cc983edabe67263864ef0fe65bf24d288ff8bc0
eduardkb <eduardbuhali@gmail.com>
2026-06-08 12:27:44 -0300

    updated update-ecs-image:

--- terraform/envs/prod/output.tf
+++ terraform/envs/prod/output.tf
@@ -5,3 +5,11 @@ output "migration_function_name" {
 output "ecr_repository_url" {
   value = module.frontend.ecr_repository_url
 }
+
+output "ecs_cluster_name" {
+  value = module.frontend.ecs_cluster_name
+}
+
+output "ecs_service_name" {
+  value = module.frontend.ecs_service_name
+}
--- terraform/modules/frontend/output.tf
+++ terraform/modules/frontend/output.tf
@@ -1,3 +1,13 @@
 output "ecr_repository_url" {
   value = aws_ecr_repository.webapp.repository_url  
 }
+
+output "ecs_cluster_name" {
+  description = "ECS Cluster Name"
+  value       = aws_ecs_cluster.webapp.name
+}
+
+output "ecs_service_name" {
+  description = "ECS Service Name"
+  value       = aws_ecs_service.webapp.name  
+}
