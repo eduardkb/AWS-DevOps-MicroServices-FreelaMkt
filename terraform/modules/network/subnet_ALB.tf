e3daab540fe90215353da4876aac09213062cb9e
eduardkb <eduardbuhali@gmail.com>
2026-06-10 14:36:48 -0300

    added cloudfront restriction

--- terraform/modules/network/subnet_ALB.tf
+++ terraform/modules/network/subnet_ALB.tf
@@ -102,16 +102,6 @@ data "aws_ec2_managed_prefix_list" "cloudfront" {
   name = "com.amazonaws.global.cloudfront.origin-facing"
 }
 
-# resource "aws_vpc_security_group_ingress_rule" "cloudfront_to_alb_http" {
-#   security_group_id = aws_security_group.alb.id
-
-#   description    = "HTTP from CloudFront only"
-#   ip_protocol    = "tcp"
-#   from_port      = 80
-#   to_port        = 80
-#   cidr_ipv4 = "0.0.0.0/0"
-# }
-
 resource "aws_vpc_security_group_ingress_rule" "cloudfront_to_alb_https" {
   security_group_id = aws_security_group.alb.id
 
@@ -119,7 +109,8 @@ resource "aws_vpc_security_group_ingress_rule" "cloudfront_to_alb_https" {
   ip_protocol    = "tcp"
   from_port      = 443
   to_port        = 443
-  cidr_ipv4 = "0.0.0.0/0"
+  
+  prefix_list_id = data.aws_ec2_managed_prefix_list.cloudfront.id
 }
 
 resource "aws_vpc_security_group_egress_rule" "alb_to_fargate" {
